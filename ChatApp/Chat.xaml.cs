a21c05cc3c2780c3c6782c8eb776e86b3a50d624
patr4586 <patr4586@ZBC-E-RO-23234>
2021-08-11 15:54:45 +0200

    commit

--- ChatApp/Chat.xaml.cs
+++ ChatApp/Chat.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ChatApp
 {
@@ -20,9 +22,61 @@ namespace ChatApp
     /// </summary>
     public partial class Chat : Page
     {
+        ChatHandler handler;
+        ListBoxItem itm;
+        Dispatcher dispatcher;
+        string Text { get; set; }
         public Chat()
         {
+            handler = new ChatHandler();
             InitializeComponent();
+            dispatcher = this.Dispatcher;
+            handler.ConnectToServer("127.0.0.1", 8000);
+            itm = new ListBoxItem();
+
+            listbox.Items.CurrentChanged += Items_CurrentChanged;
+
+            handler.ReceiveMessage += new EventHandler(Handler_ReceiveMessage);
+        }
+
+        private void Handler_ReceiveMessage(object sender, EventArgs e)
+        {
+                listbox.Items.Add($"{DateTime.Now} - {itm.Content.ToString()}");
+                listbox.SelectedIndex = listbox.Items.Count - 1;
+                listbox.ScrollIntoView(listbox.SelectedItem);
+        }
+
+        private void Items_CurrentChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            handler.SendMessage(textMessage.Text);
+            //itm.Content = handler.ReceiveMessages();
+        }
+
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                itm.Dispatcher.Invoke(new Action(() =>
+                {
+                    if (itm.Content != null)
+                    {
+                        listbox.Items.Add($"{DateTime.Now} - {itm.Content.ToString()}");
+                        listbox.SelectedIndex = listbox.Items.Count - 1;
+                        listbox.ScrollIntoView(listbox.SelectedItem);
+                    }
+                    return;
+                }));
+            }
+        }
+
+        private void listbox_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            
         }
     }
 }
---
+++ ChatApp/ChatHandler.cs
@@ -0,0 +1,67 @@
+﻿using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+    class ChatHandler
+    {
+        public event EventHandler ReceiveMessage;
+        TcpClient clientSocket;
+        NetworkStream serverStream;
+
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (value != message)
+                {
+                    message = value;
+                    ReceiveMessages();
+                }
+            }
+        }
+
+        public void ConnectToServer(string ipAdress, int port)
+        {
+            clientSocket = new TcpClient();
+
+            clientSocket.Connect(ipAdress, port);
+
+            if (null != ReceiveMessage)
+                ReceiveMessage(this, EventArgs.Empty);
+        }
+
+        public void SendMessage(string message)
+        {
+            serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(message);
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        public string ReceiveMessages()
+        {
+            EventHandler handler = ReceiveMessage;
+            if (handler != null)
+            {
+                ReceiveMessage(this, EventArgs.Empty);
+            }
+            byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+            int bytesRead = serverStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+            string data = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            //clientSocket.Close();
+
+            return data;
+        }
+    }
+}
